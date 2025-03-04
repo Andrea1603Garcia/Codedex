32bea4899c469e99dcbfb05e7b2411d235a92311
Andrea1603Garcia <melissagarciaromero57@gmail.com>
2025-03-04 15:03:17 -0600

    Primer Commit

---
+++ Desktop/Luisito2/App_Temas/Models/Calcular.cs
@@ -0,0 +1,14 @@
+﻿namespace App_Temas.Models
+{
+    public class Calcular
+    {
+
+        public int  nota1 { get; set; }
+        public int nota2 { get; set; }
+        public int nota3 { get; set; }
+        public int nota4 { get; set; }
+        public int Promedio { get; set; }
+
+
+    }
+}
